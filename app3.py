ad8d10d33d5ff1b60017152111648c8b6c665f14
KavinilaLoganathan-2024-aia <kavinilaloganathan@gmail.com>
2024-10-26 22:06:03 +0530

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+
+st.title("Hello, I'm Kavinila Loganathan!")
+st.subheader("Welcome to my portfolio")
+
+st.title("About Me")
+st.sidebar.info("""
+    Hi, I'm a beginner in web development and data science!
+    - **Email**: kavinilloganathan@gmail.com
+    - **LinkedIn**:https://www.linkedin.com/posts/kavinila-loganathan-1bb49b315_hello-everyone-im-thrilled-to-activity-7249029984193802240-SAKC?utm_source=share&utm_medium=member_android
+    - **instagram**:kavinila_loganathan
+""")
+
+st.write("I am a beginner passionate about data science.\
+    Here are some projects that I have been working on.")
+
+st.write(" Project : python and html code for food order")
+st.write("A simple python project where I explored diffrent kind of offers.")
+
+st.title(" My simple programs using python code")
+st.write("project1: python code on rock paper scissors")
+st.write("project2: python code on simple calculator")
+st.write("project3: python code on creating a list")
+st.write("project5: python code on testing Amstrong numbers")
+
+
+
+
+st.write("Made with excitement by Kavinila_loganathan")
+
